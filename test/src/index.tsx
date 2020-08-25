4862946f589b70991e6c44317faa6a61abbdf819
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-08-25 19:49:20 +0200

    Implemented simple test example displaying a counter

--- test/src/index.tsx
+++ test/src/index.tsx
@@ -2,41 +2,26 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 // @ts-ignore
 import * as CounterAction from "../bin/ordo-bindings/CounterAction.js";
+import { sleep } from "./tools/utils";
 
-import { Hello } from "./components/Hello";
+import Counter from "./components/Counter";
 
 // web worker
 const worker = new Worker("worker.js");
 
-/**worker.addEventListener("message", (e) => {
-  console.log("Received from worker: " + e.data);
-});*/
-
 import("../../ordo_adapter/pkg/ordo_adapter").then(async (ordo) => {
   const store = new ordo.Node(worker);
-
-  /**
-  const func = () => {
-    const state = store.getState();
-    console.log("This func will be unsubscribed ", state);
-  };
-  store.subscribe(func);
-  store.subscribe(() => {
-    const state = store.getState();
-    console.log("State-Change! ", state);
-  });*/
 
   await store.ready();
 
   ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <Counter compiler="TypeScript" framework="React" store={store} />,
     document.getElementById("root")
   );
 
-  const state = store.getState();
-  console.log("Ordo-Store: ", state);
-
+  await sleep(3000);
   store.dispatch(CounterAction.increment());
-  //store.unsubscribe(func);
+
+  await sleep(3000);
   store.dispatch(CounterAction.decrement());
 });
